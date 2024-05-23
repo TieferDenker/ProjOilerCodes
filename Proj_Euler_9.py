b0182d99c2d9e9bbca4a4c7321963d0589e5b83c
Angad <angadsingh1729@gmail.com>
2024-05-23 23:23:46 +0530

    Solved prob 9 correctly and completely using some help from another solved question.

--- Proj_Euler_9.py
+++ Proj_Euler_9.py
@@ -1,29 +1,29 @@
-def Solve_Prob(n):
-    if n%2 == 0:
+import itertools, math
 
-        m = n//2
+n = 3000
+# n = 100
+answers = [-1]*(2*n+1)
+
+for item in itertools.combinations(range(1,int(n**0.5)+1,2),r = 2): 
+    t = item[0]
+    s = item[1]
+    if math.gcd(s,t) == 1:
         k = 1
-        maxi = -1
-        while (k >= 1):
-            if (m%k == 0):
-                p = k
-                q = (m//k)-k
-                print(p,q)
-                if (p > q) and (q >= 1):
-                    a = p**2-q**2
-                    b = 2*p*q
-                    c = p**2+q**2
-                    pro = a*b*c
-                    if pro > maxi:
-                        maxi = pro
+        for i in range(s*(s+t),n+1,s*(s+t)):
+            abc = (s*t*(s**4-t**4))//4
+            abc = abc*k*k*k
+            answers[i] = max(answers[i],abc)
             k = k + 1
-        answer = maxi
-    else:
-        answer = -1
-    return answer
+# print(answers)
 
+def Solve_Prob(n):
+    answer = answers[n]
+    return answer
+    
 t = int(input())
+# t = 1
 for i in range(0,t):
+    # n,k = map(int, input().split())
     n = int(input())
     answer = Solve_Prob(n)
     print(answer)
