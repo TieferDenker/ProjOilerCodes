dcf5476b0e67c1881b7c5b69adc7c4ff2438292e
Angad <angadsingh1729@gmail.com>
2024-05-25 20:42:08 +0530

    Didn't solve prob 22

---
+++ Proj_Euler_22.py
@@ -0,0 +1,25 @@
+def Solve_Prob(n):
+    score1 = 0
+    for i in range(0,len(n)):
+        score1 = score1 + (ord(n[i])-64) # check this line
+    print(score1)
+    for i in Words:
+        if i == n:
+            score2 = i
+            break
+    answer = score1*score2
+    return answer
+
+Words = []
+t = int(input())
+for i in range(0,t):
+    n = input()
+    Words.append(n)
+
+Words.sort()
+
+q = int(input())
+for i in range(0,q):
+    n = input()
+    answer = Solve_Prob(n)
+    print(answer)
