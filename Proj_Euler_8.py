a3d2b8d682de03df1a96868e43fb1c7ebeffd588
Angad <angadsingh1729@gmail.com>
2024-06-09 17:12:19 +0530

    Solved prob 8. Sometimes just solve the prob without optimization.

--- Proj_Euler_8.py
+++ Proj_Euler_8.py
@@ -1,21 +1,12 @@
-eps = 0.000000000000000001
-
 def Solve_Prob(n,k,strm):
-    res = [int(x) for x in strm]
-    pro = 1
-    for i in range(0,n):
-        if res[i] == 0:
-            res[i] = eps
-    for i in range(0,k):
-        pro = pro * res[i]
-    maxi = pro
-    i = 0
-    while i < (n-k):
-        pro = (pro / res[i]) * res[i+k]
+    maxi = -1
+    for i in range(0,n-k+1):
+        pro = 1
+        for j in range(i,i+k):
+            pro = pro*int(strm[j])
         if pro > maxi:
             maxi = pro
-        i = i + 1
-    answer = int(maxi)
+    answer = maxi
     return answer
 
 t = int(input())
