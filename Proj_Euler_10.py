03775368f67965f13563de0935fc2b79b4c80e54
Angad <angadsingh1729@gmail.com>
2024-06-09 16:27:23 +0530

    Solved prob 10. NOTE: Always store the values which are being computed again and again to reduce the time complexity.

--- Proj_Euler_10.py
+++ Proj_Euler_10.py
@@ -1,50 +1,33 @@
-import random
+PRIMES = []
+SUM = []
 
-PRIMES = [2,3]
+def SieveOfEratosthenes(n):
+    global PRIMES
+    PRIMES = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        if (PRIMES[p] == True):
+            for i in range(p * p, n+1, p):
+                PRIMES[i] = False
+        p += 1
+    PRIMES[0],PRIMES[1] = False,False
 
-# Small primes for deterministic primality testing
-SMALL_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
-
-def is_prime(n, k=5):
-    if n in SMALL_PRIMES:
-        return True
-    if n <= 1 or n % 2 == 0 or n%3 == 0 or n%5 == 0 or n%7 == 0 or n%11 == 0 or n%13 == 0 or n%17 == 0 or n%19 == 0 or n%23 == 0 or n%29 == 0 or n%31 == 0 or n%37 == 0 or n%41 == 0 or n%43 == 0 or n%47 == 0:
-        return False
-
-    # Write n as d*2^r + 1
-    r, d = 0, n - 1
-    while d % 2 == 0:
-        r += 1
-        d //= 2
-
-    # Witness loop
-    for _ in range(k):
-        a = random.randint(2, n - 1)
-        x = pow(a, d, n)
-        if x == 1 or x == n - 1:
-            continue
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
-                break
-        else:
-            return False
-    return True
+def SumPrime(n):
+    global SUM
+    summ = 0
+    for i in range(0,n+1):
+        if PRIMES[i]:
+            summ = summ + i
+        SUM.append(summ)        
 
 def Solve_Prob(n):
-    i = 0
-    answer = 0
-    while PRIMES[i] <= n:
-        answer = answer + PRIMES[i]
-        i = i + 1
+    global SUM
+    answer = SUM[n]
     return answer
 
-N = int(10E5)
-for i in range(1,int((N-1)/6)+1):
-    if is_prime(6*i-1):
-        PRIMES.append(6*i-1)
-    if is_prime(6*i+1):
-        PRIMES.append(6*i+1)
+SieveOfEratosthenes(int(10E5))
+SumPrime(int(10E5))
+# print(SUM)
 
 t = int(input())
 for i in range(0,t):
