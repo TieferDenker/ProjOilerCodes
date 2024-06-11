7955cf34c7432b5981b273739421ca6f9c72a92e
Angad <angadsingh1729@gmail.com>
2024-06-11 23:39:44 +0530

    Didn't solve prob 11.

---
+++ Proj_Euler_11.py
@@ -0,0 +1,18 @@
+def Solve_Prob(n):
+    global A
+
+    return answer
+
+cols = 20
+rows = 20
+A = [[0]*cols]*rows
+# t = int(input())
+t = 1
+answer = 0
+b = []
+for i in range(0,t):
+    b = map(int, input().split())
+    # n = int(input())
+    answer = Solve_Prob(A)
+    print(answer)
+print(b)
