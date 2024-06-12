8ee878d0b42ddd6496832f6b7bd2d703792098e3
Angad <angadsingh1729@gmail.com>
2024-06-12 13:01:41 +0530

    Solved prob 11 in O(n^3)

--- Proj_Euler_11.py
+++ Proj_Euler_11.py
@@ -1,18 +1,54 @@
-def Solve_Prob(n):
-    global A
+def FindMax(B):
+    k = 4
+    l = len(B)
+    maxx = -1
+    if l-k+1 > 0:
+        for i in range(0,l-k+1):
+            prod = 1
+            for j in range(i,i+k):
+                prod = prod*B[j]
+            maxx = max(maxx,prod)
+    return maxx
 
+def Solve_Prob(A):
+    maxx = -1
+    for i in range(0,rows):
+        maxx = max(maxx,FindMax(A[i]))
+    for j in range(0,cols):
+        B = []
+        for i in range(0,rows):
+            B.append(A[i][j])
+        maxx = max(maxx,FindMax(B))
+
+    for j in range(0,cols):
+        B = []
+        for i in range(0,j+1):
+            B.append(A[i][j-i])
+        maxx = max(maxx,FindMax(B))
+    for j in range(cols,2*cols-1):
+        B = []
+        for i in range(0,2*cols-j-1):
+            B.append(A[rows-1-i][j-rows+1+i])
+        maxx = max(maxx,FindMax(B))
+
+    for j in range(0,cols):
+        B = []
+        for i in range(0,cols-j):
+            B.append(A[j+i][i])
+        maxx = max(maxx,FindMax(B))
+    for j in range(0,cols-1):
+        B = []
+        for i in range(j,cols-1):
+            B.append(A[i-j][i+1])
+        maxx = max(maxx,FindMax(B))
+    answer = maxx
     return answer
 
 cols = 20
 rows = 20
 A = [[0]*cols]*rows
-# t = int(input())
-t = 1
-answer = 0
-b = []
-for i in range(0,t):
-    b = map(int, input().split())
-    # n = int(input())
-    answer = Solve_Prob(A)
-    print(answer)
-print(b)
+
+for i in range(0,rows): 
+    A[i] = list(map(int,input().split()))
+answer = Solve_Prob(A)
+print(answer)
