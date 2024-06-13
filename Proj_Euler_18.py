015cea70ed7b87166609d1d14017f42dd33d3058
Angad <angadsingh1729@gmail.com>
2024-06-13 23:05:58 +0530

    Solved prob 18

---
+++ Proj_Euler_18.py
@@ -0,0 +1,13 @@
+def Solve_Prob(n):
+    
+    return answer
+
+
+
+t = int(input())
+for i in range(0,t):
+    
+    A = map(int,input().split())
+    n = int(input())
+    answer = Solve_Prob(n)
+    print(answer)
