c6432cc0c8555d15174771e1187456042e330068
Angad <angadsingh1729@gmail.com>
2024-06-14 22:59:09 +0530

    Solved prob 21 with hard coded values

--- Proj_Euler_21.py
+++ Proj_Euler_21.py
@@ -1,27 +1,21 @@
-def is_ami(n):
-    count = 0
-    counts = 0
-    for i in range(1,n):
-        if n%i == 0:
-            count = count + i
-    if not(count == n):
-        for i in range(1,count):
-            if count%i == 0:
-                counts = counts + i
-        if n == counts:
-            return True
-        else:
-            return False
-    else:
-        return False 
+AMINUM = [220,284,1184,1210,2620,2924,5020,5564,6232,6368,10744,10856,12285,14595,17296,18416,63020,66928,66992,67095,69615,71145,76084,79750,87633,88730]   
+AMISUM = [-1]
 
-def Solve_Prob(n):
+def GenSumAmi(n):
+    global AMINUM
+    global AMISUM
     summ = 0
     for i in range(1,n):
-        if is_ami(i):
+        if i in AMINUM:
             summ = summ + i
-    answer = summ 
+        AMISUM.append(summ)
+
+def Solve_Prob(n):
+    answer =  AMISUM[n]
     return answer
+
+N = int(10E4)
+GenSumAmi(N)
 
 t = int(input())
 for i in range(0,t):
