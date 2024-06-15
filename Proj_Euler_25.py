44c6afa717d6e9e802231a95d395fa7565b51c22
Angad <angadsingh1729@gmail.com>
2024-06-15 22:48:21 +0530

    Solved prob 25 in O(1) using the formula for Fn

--- Proj_Euler_25.py
+++ Proj_Euler_25.py
@@ -1,21 +1,13 @@
-from math import log10, floor
+from math import log10
 
 phi = 1.6180339887498948482045868343656381177203091798057628621354486227052604628189
 sq5 = 2.2360679774997896964091736687312762354406183596115257242708972454
 
 def Solve_Prob(n):
-    i = 0
-    while True:
-        Fi = round(pow(phi,i)/sq5)
-        # print(Fi)
-        if Fi == 0:
-            dig = 1
-        else:
-            dig = floor(log10(Fi))+1
-        if dig == n:
-            answer = i
-            break 
-        i=i+1
+    a = log10(sq5)
+    b = n-1
+    c = log10(phi)
+    answer = 1+int((a+b)/c)
     return answer
 
 t = int(input())
