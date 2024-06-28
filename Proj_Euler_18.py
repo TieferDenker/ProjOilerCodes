515c152b6e5cd227a58eb0a491420862cb07ca25
Angad <angadsingh1729@gmail.com>
2024-06-28 23:48:16 +0530

    Solved prob 18 using help from net

--- Proj_Euler_18.py
+++ Proj_Euler_18.py
@@ -1,13 +1,17 @@
-def Solve_Prob(n):
-    
-    return answer
-
-
+def Solve_Prob(A):
+    for i in range(len(A)-2,-1,-1):
+        l = len(A[i])
+        for j in range(0,l):
+            A[i][j] = A[i][j]+max(A[i+1][j],A[i+1][j+1])
+    return A[0][0]
 
 t = int(input())
 for i in range(0,t):
-    
-    A = map(int,input().split())
     n = int(input())
-    answer = Solve_Prob(n)
+    A = []
+    for j in range(0,n):
+        x = []
+        x = list(map(int, input().split()))
+        A.append(x)
+    answer = Solve_Prob(A)
     print(answer)
